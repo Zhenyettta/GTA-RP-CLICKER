f686e2259108c658225038d9c93b50fab6a24535
Zhenyettta <zhecky2004@gmail.com>
2023-07-06 23:41:37 +0300

    boba

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-import pyautogui
+import random
+import time
+from ctypes import windll
+
 import cv2
 import numpy as np
-import time
-import random
+import pyautogui
+import win32api
+import win32con
+from numpy.compat import unicode
 
 
 def capture_screen():
@@ -30,6 +35,39 @@ def find_template(template, image):
     return centers
 
 
+def char2key(c):
+    # https://msdn.microsoft.com/en-us/library/windows/desktop/ms646329(v=vs.85).aspx
+    result = windll.User32.VkKeyScanW(ord(unicode(c)))
+    shift_state = (result & 0xFF00) >> 8
+    vk_key = result & 0xFF
+
+    return vk_key
+
+
+def makeMove():
+    move('s')
+    time.sleep(1)
+    stop('s')
+
+    move('w')
+    time.sleep(0.4)
+    stop('w')
+
+    time.sleep(0.5)
+
+    move('e')
+    time.sleep(1.7)
+    stop('e')
+
+
+def move(c):
+    win32api.keybd_event(char2key(c), win32api.MapVirtualKey(char2key(c), 0), 0, 0)
+
+
+def stop(c):
+    win32api.keybd_event(char2key(c), win32api.MapVirtualKey(char2key(c), 0), win32con.KEYEVENTF_KEYUP, 0)
+
+
 def main_loop(template_paths):
     # Load the template images
     templates = []
@@ -40,22 +78,23 @@ def main_loop(template_paths):
         templates.append(template)
 
     while True:
-        # Capture the screen
-        frame = capture_screen()
-        # Find the center coordinates of all template images
-        for template in templates:
-            centers = find_template(template, frame)
-            if centers:
-                # Click on the center of the first match
-                center = list(centers[0])
-                for i in range(1, 6):
-                    pyautogui.click(center[0], center[1])
-                    time.sleep(i*random.uniform(0.15, 0.2))
-                    center[0] += random.randint(-3, 3)
-                    center[1] += random.randint(-3, 3)
+        makeMove()
+        for i in range(0, 11):
+            frame = capture_screen()
+            # send key down event
+            for template in templates:
+                centers = find_template(template, frame)
+                # ssssweese
+                if centers:
+                    # Click on the center of the first match
+                    center = list(centers[0])
+                    for i in range(1, 6):
+                        pyautogui.click(center[0], center[1])
+                        time.sleep(i * random.uniform(0.15, 0.15))
+                        center[0] += random.randint(-3, 3)
+                        center[1] += random.randint(-3, 3)
 
-                time.sleep(random.uniform(0.4, 1))  # Optional delay between clicks
-                break  # Move on to the next template image
+                    break  # Move on to the next template image
 
 
 if __name__ == '__main__':
